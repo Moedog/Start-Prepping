aebff1010481ef9937ff6de71e3465dd24b53fd8
James Moen <jamesmoen@yahoo.com>
2015-07-18 12:43:41 -0600

    Initial Commit

---
+++ StartPrepping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartPrepping
+//
+//  Created by Mac User on 7/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StartPrepping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartPrepping
+//
+//  Created by Mac User on 7/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
